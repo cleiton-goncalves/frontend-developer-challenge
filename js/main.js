20cc8a043e570c051ada0f7ea94e4fb21b000a5c
Cleiton Gonçalves <cleiton_goncalves@ymail.com>
2019-06-22 16:08:04 -0300

    HTML structure and API call function created

---
+++ js/main.js
@@ -0,0 +1,82 @@
+(function($){
+    $(document).ready(function () {
+        var pageNumber = 1;
+        
+        /**
+         *The createNewBoxProduct function creates the html structure of the product grid.
+         */
+        function createNewBoxProduct(product){
+            var boxProduct = document.createElement('div');
+            boxProduct.classList.add('box-product');
+
+            var boxProductImg = document.createElement('img');
+            boxProductImg.classList.add('box-product__img');
+
+            boxProduct.appendChild(boxProductImg);
+            boxProductImg.setAttribute('src','http:'+ product.image);
+
+            var boxProductName = document.createElement('p');
+            boxProductName.classList.add('box-product__name');
+            boxProductName.innerHTML=product.name;
+            boxProduct.appendChild(boxProductName);
+
+            var boxProductDescription = document.createElement('p');
+            boxProductDescription.classList.add('box-product__description');
+            boxProductDescription.innerHTML=product.description;
+            boxProduct.appendChild(boxProductDescription);
+
+            var boxProductOldPrice = document.createElement('p');
+            boxProductOldPrice.classList.add('box-product__old-price');
+            boxProductOldPrice.innerHTML=product.oldPrice;
+            boxProduct.appendChild(boxProductOldPrice);
+
+            var boxProductPrice = document.createElement('p');
+            boxProductPrice.classList.add('box-product__price');
+            boxProductPrice.innerHTML=product.price;
+            boxProduct.appendChild(boxProductPrice);
+
+            var boxProductInstallments = document.createElement('p');
+            var installmentValue = product.installments.value.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"})
+            var installmentsContent = 'ou '+product.installments.count+'x de '+ installmentValue;
+            boxProductInstallments.classList.add('box-product__installments');
+            boxProductInstallments.innerHTML=installmentsContent;
+            boxProduct.appendChild(boxProductInstallments);
+
+            var boxProductBtn = document.createElement('button');
+            boxProductBtn.classList.add('box-product__btn');
+
+            boxProduct.appendChild(boxProductBtn);
+            boxProductBtn.innerHTML='Comprar';
+
+            $('.gallery')[0].appendChild(boxProduct);
+        }
+        
+        /**
+         *The callToAPI function fetches the information from the grid and refreshes the pageNumber.
+         */
+        function callToAPI(){
+            var urlStr = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page="+pageNumber;
+            $.ajax({
+                type: "get",
+                url: urlStr,
+                data: "data",
+                dataType: "json",
+                success: function (data) {
+                    pageNumber++;
+                    $.each( data.products, function( key, product ) {
+                        createNewBoxProduct(product);
+                    });
+                }
+            });
+        }
+        callToAPI();
+
+
+        /**
+         *Load more products on the landing page.
+         */
+        $('.load-more').click(function(){
+            callToAPI();
+        })
+    });
+})(jQuery);
